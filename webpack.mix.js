c64fcde8725938117be3b1333f925dd11102670c
imCyberMohsin <mohsinansari7033@gmail.com>
2024-03-12 10:58:40 +0530

    initial

--- webpack.mix.js
+++
@@ -1,5 +0,0 @@
-let mix = require('laravel-mix')
-
-
-// (location of file to compile, location to store compiled file)
-mix.js('/resources/js/app.js', '/public/js/app.js').sass('/resources/js/app.scss', '/public/js/app.css')
