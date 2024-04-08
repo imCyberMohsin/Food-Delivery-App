843f36b7d28a410e962491e8072cc00ef95547a8
imCyberMohsin <mohsinansari7033@gmail.com>
2024-04-09 04:04:53 +0530

    cartController updated

--- app/http/controllers/customers/cartController.js
+++ app/http/controllers/customers/cartController.js
@@ -6,32 +6,42 @@ function cartController() {
             res.render('cartPage');
         },
 
-        update(req, res){
-            if(!req.session.cart){
+        update(req, res) {
+            //* Structure of data to be stored in session
+            // let cart = {
+            //     items : {
+            //         itemdId: { item: itemObject, qty:0 },
+            //     },
+            //     totalQty: 0,
+            //     totalPrice: 0,
+            // }
+
+            // Check if cart does not exists
+            if (!req.session.cart) {
                 req.session.cart = {
                     items: {},
-                    totalQty : 0,
-                    totalPrice : 0, 
+                    totalQty: 0,
+                    totalPrice: 0,
                 }
             }
             let cart = req.session.cart
 
-            // Check if item does not exists
-            if(!cart.items[req.body._id]){
+            // Check if item does not exists in cart
+            if (!cart.items[req.body._id]) {
                 cart.items[req.body._id] = {
                     item: req.body,
                     qty: 1,
                 },
-                cart.totalQty =  cart.totalQty + 1,
-                cart.totalPrice = cart.totalPrice + req.body.price
+                    cart.totalQty = cart.totalQty + 1,
+                    cart.totalPrice = cart.totalPrice + req.body.price
             } else {
                 // if already exists increase the quantity
                 cart.items[req.body._id].qty = cart.items[req.body._id].qty + 1,
-                cart.totalQty = cart.totalQty + 1,
-                cart.totalPrice = cart.totalPrice + req.body.price 
+                    cart.totalQty = cart.totalQty + 1,
+                    cart.totalPrice = cart.totalPrice + req.body.price
             }
 
-            return res.json({totalQty: req.session.cart.totalQty})
+            return res.json({ totalQty: req.session.cart.totalQty })
         }
     }
 }
